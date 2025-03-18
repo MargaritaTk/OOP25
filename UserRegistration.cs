10ccadb29e240f13ee1404357eccc20f929a93dc
Margarita Tkachenko <margosha2413@gmail.com>
2025-03-18 12:18:22 +0200

    Клас UserRegistration

---
+++ UserRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КП_ООП
+{
+    public class UserRegistration
+    {
+        private readonly List<User> _users = new List<User>();
+
+        public void AddUser(User user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            _users.Add(user);
+        }
+
+        public bool IsLoginUnique(string login)
+        {
+            return !_users.Any(u => u.Login == login);
+        }
+    }
+}
