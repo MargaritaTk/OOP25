5e3ef671d639cd2a3f5e73245fbaf25e1d16f857
Margarita Tkachenko <margosha2413@gmail.com>
2025-03-18 12:14:49 +0200

    Інтерфейс IComment

---
+++ IComment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КП_ООП
+{
+    public interface IComment
+    {
+        void AddComment(Task task, string text);
+    }
+}
