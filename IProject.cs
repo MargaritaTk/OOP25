98999edbd0b0c4c4831ae1a54158cdc08a9629e6
Margarita Tkachenko <margosha2413@gmail.com>
2025-03-18 12:15:46 +0200

    Інтерфейс IProject

---
+++ IProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КП_ООП
+{
+    public interface IProject
+    {
+        void CreateProject(string name, string description);
+        void UpdateProject(Project project, string newName, string newDescription);
+        void DeleteProject(Project project);
+    }
+}
