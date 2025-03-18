7f0d6b00d86f44217f627cf3757ed0ac94a2e855
Margarita Tkachenko <margosha2413@gmail.com>
2025-03-18 12:15:18 +0200

    Інтерфейс IExport

---
+++ IExport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КП_ООП
+{
+    using System.Text.Json;
+
+    public interface IExport
+    {
+        string ExportProjects(List<Project> projects);
+        IEnumerable<Project> ImportProjects(string jsonData);
+    }
+}
