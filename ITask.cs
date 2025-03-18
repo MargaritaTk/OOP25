06cfe0da0785db4cc82d83d83d864d82e2a9eafe
Margarita Tkachenko <margosha2413@gmail.com>
2025-03-18 12:16:13 +0200

    Інтерфейс ITask

---
+++ ITask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КП_ООП
+{
+    public interface ITask
+    {
+        Task CreateTask(string title, string description, DateTime deadline);
+        void UpdateTask(Task task, string newTitle, string newDescription, DateTime newDeadline);
+        void AssignDeveloper(Task task, Developer developer);
+        void UpdateStatus(Task task, TaskStatus newStatus);
+    }
+}
